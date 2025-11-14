d9af13828c026643188ccf96a41a9e919aae019a
Frank Hunleth <fhunleth@troodon-software.com>
2025-11-14 13:46:18 -0500

    Re-enable Phoenix live reload with Elixir 1.19.3
    This version added the export modifier to regexes in the config files to
    avoid the OTP 28 error. In fairness, if you were careful with MIX_ENV
    settings, this didn't affect you, but it was easy to get an error.

--- config/phoenix/dev.exs
+++ config/phoenix/dev.exs
@@ -47,10 +47,9 @@ config :kiosk_example, KioskExampleWeb.Endpoint,
 config :kiosk_example, KioskExampleWeb.Endpoint,
   live_reload: [
     patterns: [
-      # Temporarily disable until there's an alterative for OTP 28
-      #      ~r"priv/static/(?!uploads/).*(js|css|png|jpeg|jpg|gif|svg)$",
-      #      ~r"priv/gettext/.*(po)$",
-      #      ~r"lib/kiosk_example_web/(controllers|live|components)/.*(ex|heex)$"
+      ~r"priv/static/(?!uploads/).*(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/gettext/.*(po)$"E,
+      ~r"lib/kiosk_example_web/(controllers|live|components)/.*(ex|heex)$"E
     ]
   ]
 
